a7066f551773b839958e7d4f08fd2e73352e4dd8
abc <abc@gmail.com>
2024-02-21 17:58:41 +0530

    Add project files.

---
+++ WebAPIOne/Controllers/EmployeesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIOne.Model;
+
+namespace WebAPIOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        static List<Employee> list = new List<Employee>()
+        {
+            new Employee{ id=1,Fname="Sam",Lname="preeth",Salary =50000},
+              new Employee{ id=2,Fname="ram",Lname="reeth",Salary =50000},
+                new Employee{ id=3,Fname="rom",Lname="spreeth",Salary =50000},
+                  new Employee{ id=4,Fname="Rim",Lname="rath",Salary =50000},
+                    new Employee{ id=5,Fname="prim",Lname="mreeth",Salary =50000},
+                      new Employee{ id=6,Fname="yam",Lname="sreeeth",Salary =50000},
+        };
+        [HttpGet(Name ="GetEmployees")]
+        public IEnumerable<Employee> Get()
+        {
+            return list;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            Employee emp = list.SingleOrDefault(x=>x.id==id); 
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(emp);
+            }
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Employee> Delete(int id)
+        {
+            Employee emp = list.SingleOrDefault(x => x.id == id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                list.Remove(emp);
+                return NoContent();
+            }
+        }
+        [HttpPost]
+        public ActionResult<Employee> Post(Employee newEmp)
+        {
+            list.Add(newEmp);
+            return CreatedAtAction(nameof(Get),newEmp);
+        }
+
+        [HttpPut ("{id}")]
+        public ActionResult<Employee> Put(int id,Employee upEmp)
+        {
+            Employee existingemp = list.SingleOrDefault(x => x.id == id);
+            if (existingemp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                existingemp.Fname = upEmp.Fname;
+                existingemp.Lname = upEmp.Lname;
+                existingemp.Salary = upEmp.Salary;
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ WebAPIOne/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPIOne.Model
+{
+    public class Employee
+    {
+        public int id   { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+
+        public double Salary { get; set; }
+
+    }
+}
